b96d3032ef09ec652748e05cfc0a24fb87c75be6
AlexanderLind98 <Alexanderlind98@gmail.com>
2025-03-24 20:17:10 +0100

    Imported new asset

---
+++ Light-And-Shadow/Light-And-Shadow/Materials/mat_marble.cs
@@ -0,0 +1,22 @@
+using OpenTK_OBJ;
+using OpenTK.Mathematics;
+
+namespace Light_And_Shadow.Materials;
+
+public class mat_marble : Material
+{
+    public mat_marble() : base()
+    {
+        uniforms.Add("material.ambient", new Vector3(0.25f, 0.25f, 0.25f));
+        uniforms.Add("material.diffuse", new Vector3(0.75f, 0.75f, 0.75f));
+        uniforms.Add("material.specular", new Vector3(0.1f, 0.1f, 0.1f));
+        uniforms.Add("material.shininess", 10.0f);
+        
+        uniforms.Add("light.ambient", new Vector3(1.0f, 1.0f, 1.0f));
+        uniforms.Add("light.diffuse", new Vector3(1.0f, 1.0f, 1.0f));
+        uniforms.Add("light.specular", new Vector3(1.0f, 1.0f, 1.0f));
+        
+        UpdateUniforms();
+    }
+}
+
--- Light-And-Shadow/Light-And-Shadow/Worlds/LightTestWorld.cs
+++ Light-And-Shadow/Light-And-Shadow/Worlds/LightTestWorld.cs
@@ -18,26 +18,32 @@ public class LightTestWorld(Game game) : World(game)
         roomObject.Renderer = roomRenderer;
         GameObjects.Add(roomObject);
         
-        (GameObject cube1Object, Mesh model1Mesh) = GameObjectFactory.CreateObjModel(Game, "Cube");
-        (GameObject cube2Object, Mesh model2Mesh) = GameObjectFactory.CreateObjModel(Game, "Cube");
+        (GameObject cube1Object, Mesh model1Mesh) = GameObjectFactory.CreateObjModel(Game, "Monkey");
+        (GameObject cube2Object, Mesh model2Mesh) = GameObjectFactory.CreateObjModel(Game, "SmoothCube");
+        (GameObject StatueObject, Mesh StatueMesh) = GameObjectFactory.CreateObjModel(Game, "Statue");
 
         roomObject.Transform.Position += new Vector3(0, -2, 1);
         roomObject.Transform.Scale = new Vector3(5, 5, 5);
         cube1Object.Transform.Position += new Vector3(-2, 0, 0);
         cube2Object.Transform.Position += new Vector3(2, 0, 0);
         // cube2Object.Transform.Rotation += new Vector3(1.0f, 0.0f, 0.0f);
+        StatueObject.Transform.Scale += new Vector3(4, 4, 4);
+        StatueObject.Transform.Position += new Vector3(0, -2, -10);
 
-        // RotateObjectBehavior rotator = new RotateObjectBehavior(cube2Object, Game);
+        cube1Object.AddComponent<RotateObjectBehavior>();
         cube2Object.AddComponent<RotateObjectBehavior>();
         
         Renderer cube1Renderer = new Renderer(new mat_chrome(), model1Mesh);
         Renderer cube2Renderer = new Renderer(new mat_gold(), model2Mesh);
+        Renderer StatueRenderer = new Renderer(new mat_marble(), StatueMesh);
         
         cube1Object.Renderer = cube1Renderer;
         cube2Object.Renderer = cube2Renderer;
+        StatueObject.Renderer = StatueRenderer;
 
         GameObjects.Add(cube1Object);
         GameObjects.Add(cube2Object);
+        GameObjects.Add(StatueObject);
         
         //game.Title = "Light Test";
     }
